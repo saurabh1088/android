792fecf43555f2f788de8fde2df54594d61a6ecd
Saurabh Verma <saurabh1088@gmail.com>
2023-07-30 23:16:13 +0530

    Started introduction to android development.

--- app/src/main/java/com/example/androidcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidcompose/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidcompose.ui.theme.AndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    /*
+     onCreate below is the entry point for Android app. Kotlin programs have main() function as the
+     entry point. In case of Android apps onCreate() function fills the role of entry point.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,12 +39,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/*
+@Composable annotation tells Kotlin compiler that this function will be used by Jetpack Compose to
+generate some UI as declared by it.
+One interesting point about functions annotated with @Composable is that the naming convention followed
+is such that the names are capitalized.
+
+Functions annotated with @Composable :
+- Names are capitalized
+- Can't return anything
+ */
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
