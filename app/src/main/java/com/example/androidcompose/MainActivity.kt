437b88bb2e2a703d36af97e7bf92bb96bb57bf2a
Saurabh Verma <saurabh1088@gmail.com>
2023-08-03 23:00:59 +0530

    Modified as per following pathway for compose learning.

--- app/src/main/java/com/example/androidcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidcompose/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.androidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidcompose.ui.theme.AndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +36,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // TODO: Fix this
-                    Greeting("Android")
-                    Greeting("Hello World!")
+                    GreetingText(
+                        message = "Happy Birthday, Batman",
+                        from = "From, Man of Steel"
+                    )
                 }
             }
         }
@@ -42,8 +49,9 @@ class MainActivity : ComponentActivity() {
 /*
 @Composable annotation tells Kotlin compiler that this function will be used by Jetpack Compose to
 generate some UI as declared by it.
-One interesting point about functions annotated with @Composable is that the naming convention followed
-is such that the names are capitalized.
+One interesting point about functions annotated with @Composable is that the naming convention
+followed is such that the names are capitalized. This convention is called pascal case and this MUST
+be followed for all composable functions.
 
 Functions annotated with @Composable :
 - Names are capitalized
@@ -63,10 +71,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidComposeTheme {
-        Greeting("Android")
+        GreetingText(message = "Hello Batman!", from = "Superman")
     }
 }
