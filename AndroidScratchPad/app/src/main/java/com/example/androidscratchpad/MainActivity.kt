42f9c8ba28b33d4d6bcdbd84ddc71a6229b0b33e
Saurabh Verma <saurabh1088@gmail.com>
2026-02-07 14:14:37 +0530

    Added OOTB FloatingActionButton component.

--- AndroidScratchPad/app/src/main/java/com/example/androidscratchpad/MainActivity.kt
+++ AndroidScratchPad/app/src/main/java/com/example/androidscratchpad/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Badge
@@ -20,6 +21,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
@@ -55,6 +57,16 @@ class MainActivity : ComponentActivity() {
                                 Text("My Scratch-pad App")
                             }
                         )
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = { println("FAB Clicked") }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = "Add"
+                            )
+                        }
                     }
                 ) { innerPadding ->
                     Column(
